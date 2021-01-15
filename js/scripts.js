549ce6426fa2d6000c7ce67c54acdaaa2ff593e1
dcouch440 <dcouch440@gmail.com>
2021-01-15 11:17:06 -0800

    add html id's to desired conditional effects

--- js/scripts.js
+++ js/scripts.js
@@ -44,14 +44,22 @@ const getRoot = () => {
 getRoot()
 
 $(document).ready(function() {
-  $("#submit-button").click(function(event) {
+  $('#submit-button').click(function(event) {
     event.preventDefault()
     const input1_store = parseInt($("input:radio[name=radioButton-1]:checked").val());
     const input2_hair = parseInt($("input:radio[name=radioButton-2]:checked").val());
     const input3_micraphone = parseInt($("input:radio[name=radioButton-3]:checked").val()); 
     const input4_bottle = parseInt($("input:radio[name=radioButton-4]:checked").val());
     const input5_dog = parseInt($("input:radio[name=radioButton-5]:checked").val());
-    
-    const totalValue = input1_store + input2_hair + input3_micraphone + input5_dog;
+    const totalValue = input1_store + input2_hair + input3_micraphone + input4_bottle + input5_dog;
+    $('#greet-message').hide()
+    $('#submit-button').hide()
+    if (totalValue > 15) {
+      $('#ruby').show()
+    } else if (totalValue > 7) {
+      $('#react').show()
+    } else {
+      $('#html').show()
+    }
   })
 })
