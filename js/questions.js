9ed846a6998bb35e87f161f4e3e07c92915bddeb
dcouch440 <dcouch440@gmail.com>
2021-01-15 09:41:36 -0800

    add functioning string map with join in scripts file

--- js/questions.js
+++
@@ -1,7 +0,0 @@
-export const questions = [
-  "A walk to the store should take three or more hours", 
-  "A comb is better than a brush", 
-  "A micraphone is just a speaker in reverse",
-  "A plastic bottle weighs as much as a glass bottle",
-  "A dog is as fast as a cheetah"
-]
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,40 @@
+const questionMapper = () => {
+  const questions = [
+    "A walk to the store should take three or more hours", 
+    "A comb is better than a brush", 
+    "A micraphone is just a speaker in reverse",
+    "A plastic bottle weighs as much as a glass bottle",
+    "A dog is as fast as a cheetah"
+  ]
+  const questionMap = () => {
+    let iteration = 0
+    return questions.map(question => {
+      iteration += 1
+      return `
+        <p class="question-title">${question}</p>
+        <div class="radio-button-cluster">
+          <div class="radio-button-row">
+            <input type="radio" name="radioButton-${iteration}" id="radios1" value="option1" checked>
+            <label class="form-check-label" for="radios1">Agree Greatly</label>
+          </div>
+          <div class="radio-button-row">
+            <input type="radio" name="radioButton-${iteration}" id="radios2" value="option1">
+            <label class="form-check-label" for="radios2">Agree Greatly</label>
+          </div>
+          <div class="radio-button-row">
+            <input type="radio" name="radioButton-${iteration}" id="radios3" value="option1">
+            <label class="form-check-label" for="radios3">Agree Greatly</label>
+          </div>
+          <div class="radio-button-row">
+            <input type="radio" name="radioButton-${iteration}" id="radios4" value="option1">
+            <label class="form-check-label" for="radios4">Agree Greatly</label>
+          </div>
+        </div>
+      `
+    })
+  }
+  return questionMap().join('')
+}
+
 $(document).ready(function() {
-  
 });
