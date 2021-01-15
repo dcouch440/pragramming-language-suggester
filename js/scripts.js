57c2905a477437f1cdb9082503402d4646c99fd7
dcouch440 <dcouch440@gmail.com>
2021-01-15 14:36:16 -0800

    add additional english to response section

--- js/scripts.js
+++ js/scripts.js
@@ -45,12 +45,12 @@ getRoot()
 $(document).ready(function() {
   $('#submit-button').click(function(event) {
     event.preventDefault()
-    const input1_store = parseInt($("input:radio[name=radioButton-1]:checked").val());
-    const input2_hair = parseInt($("input:radio[name=radioButton-2]:checked").val());
-    const input3_micraphone = parseInt($("input:radio[name=radioButton-3]:checked").val()); 
-    const input4_bottle = parseInt($("input:radio[name=radioButton-4]:checked").val());
-    const input5_dog = parseInt($("input:radio[name=radioButton-5]:checked").val());
-    const totalValue = input1_store + input2_hair + input3_micraphone + input4_bottle + input5_dog;
+    const input1_store = parseInt($("input:radio[name=radioButton-1]:checked").val())
+    const input2_hair = parseInt($("input:radio[name=radioButton-2]:checked").val())
+    const input3_micraphone = parseInt($("input:radio[name=radioButton-3]:checked").val())
+    const input4_bottle = parseInt($("input:radio[name=radioButton-4]:checked").val())
+    const input5_dog = parseInt($("input:radio[name=radioButton-5]:checked").val())
+    const totalValue = input1_store + input2_hair + input3_micraphone + input4_bottle + input5_dog
     
     $('#greet-message').hide()
     $('#submit-button').hide()
@@ -67,6 +67,6 @@ $(document).ready(function() {
     }
   })
   $('#start-over-button').click(function(){
-    location.reload();
+    location.reload()
   })
 })
