9504a2b46a2c2c17f3608be465fab4d1af476b6c
dcouch440 <dcouch440@gmail.com>
2021-01-16 16:21:20 -0800

    add module routing to file tree and additional map hierarchy for automation

---
+++ js/questionnaire.js
@@ -0,0 +1,41 @@
+const radioButton = (label, index, value) => {
+  
+  // value is drawn from the iteration
+  
+  const isFirst = value === 1 ? 'checked' : null
+  return `
+    <div class="radio-button-row">
+      <input type="radio" name="radioButton-${index}" value="${value}" ${isFirst}>
+      <label class="form-check-label" for="radios1">${label}</label>
+    </div>`
+}
+export const questionMapper = () => {
+  const questions = [
+    'A walk to the store should take three or more hours', 
+    'A comb is better than a brush', 
+    'A micraphone is just a speaker in reverse',
+    'A plastic bottle weighs as much as a glass bottle',
+    'A dog is as fast as a cheetah'
+  ]
+  const inputs = [
+    'Agree Greatly',
+    'Agree Kinda',
+    'Disagree Kinda',
+    'Disagree Greeatly'
+  ]
+  const questionMap = () => {
+    return questions.map((question, questionsindex) => {
+      questionsindex += 1
+      return `
+        <div class="question-container">
+          <p class="question-title">${question}</p>
+          <form class="radio-button-cluster">
+            ${inputs.map(
+              (input, value) => radioButton(input, questionsindex, value + 1)).join("")
+            }
+          </form>
+        </div>`
+    })
+  }
+  return questionMap().join('')
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,8 @@
-const questionMapper = () => {
-  const questions = [
-    "A walk to the store should take three or more hours", 
-    "A comb is better than a brush", 
-    "A micraphone is just a speaker in reverse",
-    "A plastic bottle weighs as much as a glass bottle",
-    "A dog is as fast as a cheetah"
-  ]
-  const questionMap = () => {
-    let iteration = 0
-    return questions.map(question => {
-      iteration += 1
-      return `
-        <div class="question-container">
-          <p class="question-title">${question}</p>
-          <form class="radio-button-cluster">
-            <div class="radio-button-row">
-              <input type="radio" name="radioButton-${iteration}" value="1" checked>
-              <label class="form-check-label" for="radios1">Agree Greatly</label>
-            </div>
-            <div class="radio-button-row">
-              <input type="radio" name="radioButton-${iteration}" value="2">
-              <label class="form-check-label" for="radios2">Agree Kinda</label>
-            </div>
-            <div class="radio-button-row">
-              <input type="radio" name="radioButton-${iteration}" value="3">
-              <label class="form-check-label" for="radios3">Disagree Kinda</label>
-            </div>
-            <div class="radio-button-row">
-              <input type="radio" name="radioButton-${iteration}" value="4">
-              <label class="form-check-label" for="radios4">Disagree Greatly</label>
-            </div>
-          </form>
-        </div>`
-    })
-  }
-  return questionMap().join('')
-}
+import * as utils from './questionnaire.js'
+
 const getRoot = () => {
   const rootId = document.getElementById('questionnaire')
-  rootId.innerHTML = questionMapper()
+  rootId.innerHTML = utils.questionMapper()
 }
 getRoot()
 
